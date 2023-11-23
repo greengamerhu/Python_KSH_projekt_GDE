c684b9591af05588bb9f0113961ea03b0d7e542b
stickyfox0101 <matesarosi1@gmail.com>
2023-11-23 18:31:36 +0100

    commenteles

--- adatbeolvasas.py
+++ adatbeolvasas.py
@@ -1,11 +1,17 @@
 def adat():
+    # fájl megnyitása beolvasásra
     f = open("adatok.csv", "r")
+    # grafikon címe a fájl első sora
     cim = f.readline()
+    # táblázat fejlécei
     header = f.readline() + f.readline() + f.readline()
+    # táblázat összessége
     adatok = []
+    # végig megyek a fájl sorain
     for sor in f:
         adatok.append((sor.replace("..", "0").replace(" ", "").rstrip().split(";")))
-    vezetekes_osszesen = {}
+        # A sorok listává alakítása és formázása, .. helyett 0, szóközök eltávolítása, majd listában eltárolás
+    vezetekes_osszesen = {}  # Létrehozunk dicteket(oszlopok nevei).
     vezetekes_kapcsolt_vonal = {}
     xdsl_halozaton = {}
     kabeltv_halozaton = {}
@@ -13,6 +19,8 @@ def adat():
     vezetek_nelkuli_halozaton = {}
     osszesen = {}
     for item in adatok:
+        # végigmegyek a listákat tartalmazó listán és kiveszem az első elemet ami mindig az évszám,
+        # a masodik pedig az oszlophoz tartozó adat
         vezetekes_osszesen[item[0]] = int(item[1])
         vezetekes_kapcsolt_vonal[item[0]] = int(item[2])
         xdsl_halozaton[item[0]] = int(item[3])
@@ -21,6 +29,7 @@ def adat():
         vezetek_nelkuli_halozaton[item[0]] = int(item[6])
         osszesen[item[0]] = int(item[7])
     try:
+        # Megpróbáljuk kiírni a szótárak tartalmát.
         print(vezetekes_osszesen)
         print(vezetekes_kapcsolt_vonal)
         print(xdsl_halozaton)
@@ -29,17 +38,23 @@ def adat():
         print(vezetek_nelkuli_halozaton)
         print(osszesen)
     except FileNotFoundError:
+        # Ha a fájl nem található, akkor kiírjuk ezt a hibaüzenetet.
         print("A fajl nem találhato. ")
     except ValueError:
+        # Ha a fájlban lévő adatok nem megfelelőek, akkor kiírjuk ezt a hibaüzenetet.
         print("Nem megfelelo adatok a fajlban.")
     except PermissionError:
+        # Ha nincs jogosultságunk a fájl megnyitására, akkor kiírjuk ezt a hibaüzenetet.
         print("Nincs jogosultsag a fajl megnyitásara. ")
     except Exception as e:
+        # Ha bármilyen más hiba történik, akkor kiírjuk ezt a hibaüzenetet.
         print("Valami hiba tortent: ", e)
     else:
+        # Ha nincs hiba, akkor kiírjuk, hogy a fájl sikeresen beolvasva.
         print("A file sikeresen beolvasva. ")
     finally:
         print("A kod veget ert.")
+        # Végül kiírjuk, hogy a kód véget ért.
         return vezetekes_osszesen, vezetekes_kapcsolt_vonal, xdsl_halozaton, kabeltv_halozaton, optikai_halozaton, vezetek_nelkuli_halozaton, osszesen
 
 
